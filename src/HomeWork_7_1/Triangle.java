9719c4fc85f5d5711d4d52a28479644a6569a1a7
Admin <savenokslava@gmail.com>
2024-05-23 04:41:39 +0300

    Created a hierarchy of the shape class, created an array (size from the console) and displayed the sum of the perimeters of the shapes in the array.

---
+++ src/HomeWork_7_1/Triangle.java
@@ -0,0 +1,58 @@
+package HomeWork_7_1;
+
+public class Triangle implements CalculatePerimeter,CalculateSquar {
+    double sideA;
+    double sideB;
+    double sdeC;
+    double height;
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSdeC() {
+        return sdeC;
+    }
+
+    public void setSdeC(double sdeC) {
+        this.sdeC = sdeC;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Triangle(double sideA, double sideB, double sdeC, double height) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sdeC = sdeC;
+        this.height = height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return sideA+sideB+sdeC;
+
+    }
+
+    @Override
+    public double getSquar() {
+        return  sideA /2*height;
+    }
+}
