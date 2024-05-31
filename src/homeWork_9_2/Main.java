4dd3540f6fa723a8aee1987fd46b984487c9487a
Admin <savenokslava@gmail.com>
2024-05-31 14:25:52 +0300

    Created a class with a static method that accepts parameters (login, password, confirmPassword). Created classes with two constructors: WrongPasswordException and WrongLoginException. If password or login does not meet the requirements, I throw exceptions

---
+++ src/homeWork_9_2/Main.java
@@ -0,0 +1,25 @@
+package homeWork_9_2;
+
+public class Main {
+    public static void main(String[] args) {
+        String login;
+        String password;
+        String confirmPassword;
+
+    }
+    public boolean CheckException(String login, String password, String confirmPassword) throws WrongPasswordException {
+        try {
+            if (login.length() > 20 || login.contains(" ")) {
+                throw new WrongLoginException();
+            }
+
+            if (password.length() > 20 || password.contains(" ") || !password.contains("1") || password.equals(confirmPassword))
+                throw new WrongPasswordException();
+            return true;
+
+        } catch (WrongPasswordException | WrongLoginException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+}
---
+++ src/homeWork_9_2/WrongPasswordException.java
@@ -0,0 +1,14 @@
+package homeWork_9_2;
+
+public class WrongPasswordException extends Throwable {
+    public WrongPasswordException() {
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
+
+
+
+
