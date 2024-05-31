882757597bd92adb861c80a105a5ba3a8d31c52b
Admin <savenokslava@gmail.com>
2024-05-31 14:37:27 +0300

    corrected

--- src/homeWork_9_2/Main.java
+++ src/homeWork_9_2/Main.java
@@ -7,13 +7,13 @@ public class Main {
         String confirmPassword;
 
     }
-    public boolean CheckException(String login, String password, String confirmPassword) throws WrongPasswordException {
+    public boolean CheckException(String login, String password, String confirmPassword) throws WrongPasswordException, WrongLoginException {
         try {
             if (login.length() > 20 || login.contains(" ")) {
                 throw new WrongLoginException();
             }
 
-            if (password.length() > 20 || password.contains(" ") || !password.contains("1") || password.equals(confirmPassword))
+            if (password.length() > 20 || password.contains(" ") || !password.contains("1") || !password.equals(confirmPassword))
                 throw new WrongPasswordException();
             return true;
 
