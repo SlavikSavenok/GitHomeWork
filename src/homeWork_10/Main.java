e1c3f9c3446ce3ee1c654a8e41b1c3cb9d7bc53c
Admin <savenokslava@gmail.com>
2024-06-03 23:51:40 +0300

    Сorrected

--- src/homeWork_10/Main.java
+++ src/homeWork_10/Main.java
@@ -2,14 +2,14 @@ package homeWork_10;
 
 public class Main extends StringMethods {
     public static void main(String[] args) {
-        String dokument = "4820-aPs-4871-Odh-1a2b";
+        String doсument = "4820-aPs-4871-Odh-1a2b";
 
-        System.out.println("two blocks with numbers: " + getBlocks(dokument));
-        System.out.println("block replacement: " + getReplace(dokument));
-        System.out.println("document letters: " + getLetters(dokument));
-        System.out.println("does the document number begin with 555: " + getStart(dokument));
-        System.out.println("does the document number end with 1a2b: " + getEnd(dokument));
-        System.out.println("is there a sequence (abc): " + getSubsequence(dokument));
-        System.out.println(getBigLetters(dokument));
+        System.out.println("two blocks with numbers: " + getBlocks(doсument));
+        System.out.println("block replacement: " + getReplace(doсument));
+        System.out.println("document letters: " + getLetters(doсument));
+        System.out.println("does the document number begin with 555: " + getStart(doсument));
+        System.out.println("does the document number end with 1a2b: " + getEnd(doсument));
+        System.out.println("is there a sequence (abc): " + getSubsequence(doсument));
+        System.out.println(getBigLetters(doсument));
     }
 }
--- src/homeWork_10/StringMethods.java
+++ src/homeWork_10/StringMethods.java
@@ -2,36 +2,36 @@ package homeWork_10;
 
 public class StringMethods {
 
-    String dokument;
+    String doсument;
 
-    public static boolean getStart(String dokument) {
-        return dokument.startsWith("555");
+    public static boolean getStart(String doсument) {
+        return doсument.startsWith("555");
     }
 
-    public static boolean getEnd(String dokument) {
-        return dokument.endsWith("1a2b");
+    public static boolean getEnd(String doсument) {
+        return doсument.endsWith("1a2b");
     }
 
-    public static String getReplace(String dokument) {
-        return dokument.replace("aPs", "***").replace("Odh", "***");
+    public static String getReplace(String doсument) {
+        return doсument.replace("aPs", "***").replace("Odh", "***");
     }
 
-    public static String getBlocks(String dokument) {
-        String blockOne = dokument.substring(0, 5);
-        String blockTwo = dokument.substring(9, 13);
+    public static String getBlocks(String doсument) {
+        String blockOne = doсument.substring(0, 5);
+        String blockTwo = doсument.substring(9, 13);
         return blockOne + blockTwo;
     }
 
-    public static String getLetters(String dokument) {
-        String letters = dokument.substring(5, 8) + "/" + dokument.substring(14, 17) + "/" + dokument.charAt(19) + "/" + dokument.charAt(21);
+    public static String getLetters(String doсument) {
+        String letters = doсument.substring(5, 8) + "/" + doсument.substring(14, 17) + "/" + doсument.charAt(19) + "/" + doсument.charAt(21);
         return letters.toLowerCase();
     }
 
-    public static boolean getSubsequence(String dokument) {
-        return dokument.contains("adc");
+    public static boolean getSubsequence(String doсument) {
+        return doсument.equalsIgnoreCase(String.valueOf(doсument.contains("abc")));
     }
 
-    public static String getBigLetters(String dokument) {
+    public static String getBigLetters(String doсument) {
         StringBuilder stringBuilder;
         stringBuilder = new StringBuilder("4820-aPs-4871-Odh-1a2b");
         stringBuilder.delete(0, 5).delete(3, 9).delete(6, 8).deleteCharAt(7);
